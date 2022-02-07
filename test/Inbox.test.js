d1ad80bda21b1b34e2e065351617323b497426f4
Benoît Le Goff <benoit.le.goff@icloud.com>
2022-02-07 20:56:11 +0100

    Refine some value

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,12 +9,13 @@ let fetchedAccounts;
 let inboxContract;
 let __INITIAL__MESSAGE = 'Ping!';
 let __UPDATED__MESSAGE = 'Pong!';
+let __TEST__GAS__VALUE = 1000000;
 
-beforeEach('Get Eth accounts', async() => {
+beforeEach('Get Ethereum accounts from ganache and deploy contract', async () => {
     fetchedAccounts = await web3.eth.getAccounts();
     inboxContract = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({data: bytecode, arguments: [__INITIAL__MESSAGE]} )
-        .send({from: fetchedAccounts[0], gas: '1000000'});
+        .send({from: fetchedAccounts[0], gas: __TEST__GAS__VALUE});
 });
 
 describe('Inbox Contract', () => {
@@ -23,11 +24,13 @@ describe('Inbox Contract', () => {
         assert.equal(message, __INITIAL__MESSAGE);
     });
     it('Set message value', async () => {
-        let response = await inboxContract.methods.setMessage(__UPDATED__MESSAGE).send({from: fetchedAccounts[0], gas: '1000000'});
+        let response = await inboxContract.methods.setMessage(__UPDATED__MESSAGE).send({from: fetchedAccounts[0], gas: __TEST__GAS__VALUE});
+        // Contrôle de présence du hash de transaction
         assert.ok(response.transactionHash);
     });
     it('Get updated message value', async () => {
-        await inboxContract.methods.setMessage(__UPDATED__MESSAGE).send({from: fetchedAccounts[0], gas: '1000000'});
+        // Ici c'est un transaction avec consommation de gas car update d'une valeur.
+        await inboxContract.methods.setMessage(__UPDATED__MESSAGE).send({from: fetchedAccounts[0], gas: __TEST__GAS__VALUE});
         let message = await inboxContract.methods.message().call();
         assert.equal(message, __UPDATED__MESSAGE);
     });
